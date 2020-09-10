24e305e86a5dd00bef4eb4ec2b96bdf395d215d3
CommanderRedYT <ccommanderred@gmail.com>
2020-09-10 18:41:42 +0200

    Added localhost support

--- js/addToURL.js
+++ js/addToURL.js
@@ -1,6 +1,6 @@
 function addHTML() {
 
-    if (window.location.protocol == 'file:') {
+    if (window.location.protocol == 'file:' || window.location.hostname == '127.0.0.1') {
         var filename = document.getElementById("addhtml")
         filename += ".html"
         document.getElementById("addhtml").href = filename;
@@ -11,7 +11,7 @@ function addHTML() {
 
 function addIndex() {
 
-    if (window.location.protocol == 'file:') {
+    if (window.location.protocol == 'file:' || window.location.hostname == '127.0.0.1') {
         var filename = document.getElementById("addindex")
         filename += "index.html"
         document.getElementById("addindex").href = filename;
